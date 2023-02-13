2ed82e31194cf062b8b9a678c34c2e97fe85bc06
rooney <roone858@gmail.com>
2023-02-14 00:19:27 +0200

    add circle file

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -2,7 +2,8 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 // ENV variables 
-
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
 export const config = {
@@ -12,6 +13,8 @@ export const config = {
   host: process.env.POSTGRES_HOST,
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
+  aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+  aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
